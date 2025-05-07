469de536198b52262a746c985e24685902357a3e
João Victor <ftcparamim@yahoo.com.br>
2025-05-07 08:31:14 -0300

    feat(pagination): adicionado o número de pagina atual e o total

--- backend/src/main/java/com/drillmap/backend/dtos/PocoDTO.java
+++ backend/src/main/java/com/drillmap/backend/dtos/PocoDTO.java
@@ -2,6 +2,8 @@ package com.drillmap.backend.dtos;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,9 +22,16 @@ public class PocoDTO {
     private Double longitude;
     private String estado;
     private String situacao;
-    private LocalDate inicio;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate inicio; 
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate termino;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate conclusao;
+
     private String reclassificacao;
     private String tipodePoco;
     private String categoria;
